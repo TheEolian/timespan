9417b53025547b673b743161afd93b69bbc3c8d5
Michael Soldmann <msoldmann@gmail.com>
2022-06-30 13:55:13 -0500

    Set Timespan for Bi-Annual Run

--- Timespan.py
+++ Timespan.py
@@ -1,4 +1,6 @@
+##########################################################################
 # Setting Timespan for Annual Job Run
+##########################################################################
 
 from datetime import datetime
 
@@ -8,8 +10,9 @@ priorYear = currentYear - 1
 start_date = f"{priorYear}-01-01"
 end_date = f"{priorYear}-12-31"
 
-
+##########################################################################
 # Setting Timespan for Quarterly Job Run
+##########################################################################
 
 from datetime import datetime
 
@@ -33,5 +36,26 @@ elif (currentMonth > 6) & (currentMonth < 10):
 elif currentMonth > 9:
     start_date = f"{currentYear}-07-01"
     end_date = f"{currentYear}-10-31"
+else:
+    print("Error Setting Date Range")
+
+##########################################################################
+# BI-ANNUAL RUN
+##########################################################################
+
+# Run Half prior to Current Half
+
+from datetime import datetime
+
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
+priorYear = currentYear - 1
+
+if currentMonth < 7:
+    start_date = f"{priorYear}-07-01"
+    end_date = f"{priorYear}-12-31"
+elif currentMonth > 7:
+    start_date = f"{currentYear}-01-01"
+    end_date = f"{currentYear}-06-30"
 else:
     print("Error Setting Date Range")
